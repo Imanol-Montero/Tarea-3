52162063dfd07464e7a8073d79d10d48aa0908a0
Imanol-Montero <79027212+Imanol-Montero@users.noreply.github.com>
2021-02-13 23:32:45 -0400

    Add files via upload

---
+++ Programa 1/Progrma1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0;
+            int b=1;
+
+            while (b > 0)
+            {
+                Console.WriteLine("Numero Mayor: a 0");
+                b = int.Parse(Console.ReadLine());
+                a = a + b;
+                Console.Clear();
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
