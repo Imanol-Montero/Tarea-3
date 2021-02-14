37f540d6ed9c054d36c998198bd295746d34ce2c
Imanol-Montero <20197920@itla.edu.do>
2021-02-13 22:41:21 -0400

    cambio

---
+++ Programa 2/Programa2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2World!");
+        }
+    }
+}
