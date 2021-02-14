da343bd76f31e0c2f8e4f980c395a00e739a47d6
Imanol-Montero <20197920@itla.edu.do>
2021-02-13 23:24:17 -0400

    Programa 3

--- Programa 3/Programa3.cs
+++ Programa 3/Programa3.cs
@@ -6,7 +6,25 @@ namespace Programa_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int a = 10 * 10;
+            Double b = 0;
+            int c = 0;
+            
+            while (c < 11)
+            {
+                if (b == 0.00)
+                {
+
+                    Console.WriteLine(a);
+                    
+                    b = a % 2;
+                    
+                }
+
+                a += 1;
+                c += 1;
+            }
+            Console.ReadKey();
         }
     }
 }
